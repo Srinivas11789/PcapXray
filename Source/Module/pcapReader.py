453b01eb5d493943a66b0c33832a8b81bee135bd
Srinivas <spg349@nyu.edu>
2017-11-20 20:56:46 -0500

    scapy packet reader

--- Source/Module/pcapReader.py
+++ Source/Module/pcapReader.py
@@ -9,26 +9,34 @@ class pcapReader():
         # pcap file handle
         self.packets = rdpcap(filename)
 
-    def fetch_specific_protocol(self, protocol):
+    def fetch_specific_protocol(self, layer, protocol):
         # Packet Iteration
-        for packet in self.packets:
-            if packet.haslayer(protocol):
-                if isinstance(packet.answer, protocol):
-                    return packet.answer
-                else:
-                    return None
-
-
+        if protocol == "HTTP":
+            port = 80
+        elif protocol == "HTTPS":
+            port = 443
+        else:
+            port = None
+        for session in self.packets.sessions():
+            for packet in self.packets.sessions()[session]:
+                    if layer.upper() == "TCP":
+                        if (packet.haslayer(TCP)):
+                            if packet[TCP].dport == port or packet[TCP].sport == port:
+                                    if packet.haslayer(Raw):
+                                        payload = packet.getlayer(Raw).load
+                                        print "\n".join(packet.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n"))
+                                        #for item in payloads:
+                                        #    print item.decode('hex')
+                            else:
+                                    return None
 
 # Module Driver
-
 def main():
     pcapfile = pcapReader('test.pcap')
-    print pcapfile.fetch_specific_protocol("HTTP")
+    print pcapfile.fetch_specific_protocol("TCP","HTTPS")
 
+main()
 
-def __init__():
-    main()
 
 
 
