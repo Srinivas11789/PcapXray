d748ada2cb548b90e4a87f882d3a2431c2e98fa5
Srinivas <spg349@nyu.edu>
2017-11-30 16:17:44 -0500

    source address fetch from pcap

--- Source/Module/pcapReader.py
+++ Source/Module/pcapReader.py
@@ -10,12 +10,14 @@ class pcapReader():
         self.packets = rdpcap(filename)
         self.packetDB = {}
         self.payloadExchange = {}
+        self.server_addresses = {}
 
 
     def fetch_specific_protocol(self, layer, protocol):
         # Protocol Packet Store
         self.packetDB[protocol] = []
         self.payloadExchange[protocol] = []
+        self.server_addresses[protocol] = []
         # Protocol with Well Known Ports
         if protocol == "HTTP":
             port = 80
@@ -29,12 +31,13 @@ class pcapReader():
             for each_session in sessions[session]:
                 for packet in each_session:
                     #print packet.show()
-                    if (packet.haslayer(TCP)):
-                        if packet[TCP].dport == port or packet[TCP].sport == port:
+                    if (packet.haslayer(layer)):
+                        if packet[layer].dport == port or packet[layer].sport == port:
                             self.packetDB[protocol].append(packet)
                             if packet.haslayer(Raw):
-                                payload = packet.getlayer(Raw).load
                                 self.payloadExchange[protocol].append("\n".join(packet.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n")))
+                        if packet[layer].dport == port:
+                            self.server_addresses[protocol].append(packet.getlayer(IP).src)
                     else:
                         return None
 
@@ -42,7 +45,7 @@ class pcapReader():
 def main():
     pcapfile = pcapReader('test.pcap')
     pcapfile.fetch_specific_protocol("TCP","HTTP")
-    print pcapfile.payloadExchange
+    print pcapfile.server_addresses
 
 
 main()
