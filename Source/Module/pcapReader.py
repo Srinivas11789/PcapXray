415decda50ee48eb3b40fd55b43ce5272a53b047
Srinivas <spg349@nyu.edu>
2017-11-30 14:54:24 -0500

    protocol packet fetch and payload fetch

--- Source/Module/pcapReader.py
+++ Source/Module/pcapReader.py
@@ -8,37 +8,38 @@ class pcapReader():
     def __init__(self, filename):
         # pcap file handle
         self.packets = rdpcap(filename)
+        self.packetDB = {}
+        self.payloadExchange = {}
+
 
     def fetch_specific_protocol(self, layer, protocol):
-        # Packet Iteration
+        # Protocol Packet Store
+        self.packetDB[protocol] = []
+        self.payloadExchange[protocol] = []
+        # Protocol with Well Known Ports
         if protocol == "HTTP":
             port = 80
         elif protocol == "HTTPS":
             port = 443
         else:
             port = None
+        # Packet Iteration
         for session in self.packets.sessions():
-            for packet in self.packets.sessions()[session]:
-                    if layer.upper() == "TCP":
-                        if (packet.haslayer(TCP)):
-                            if packet[TCP].dport == port or packet[TCP].sport == port:
-                                    if packet.haslayer(Raw):
-                                        payload = packet.getlayer(Raw).load
-                                        print "\n".join(packet.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n"))
-                                        #for item in payloads:
-                                        #    print item.decode('hex')
-                            else:
-                                    return None
+            for each_session in session:
+                for packet in each_session:
+                    #print packet.show()
+                    if (packet.haslayer(TCP)):
+                        if packet[TCP].dport == port or packet[TCP].sport == port:
+                            self.packetDB[protocol].append(packet)
+                            if packet.haslayer(Raw):
+                                payload = packet.getlayer(Raw).load
+                                self.payloadExchange[protocol].append("\n".join(packet.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n")))
+                    else:
+                        return None
 
 # Module Driver
 def main():
     pcapfile = pcapReader('test.pcap')
-    print pcapfile.fetch_specific_protocol("TCP","HTTPS")
+    print pcapfile.fetch_specific_protocol("TCP","HTTP")
 
 main()
-
-
-
-
-
-
