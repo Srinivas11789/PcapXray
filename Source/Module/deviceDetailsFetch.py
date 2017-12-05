f0d74ef4a14788196771bc9bcfc52b2500dee412
Srinivas <spg349@nyu.edu>
2017-12-05 13:44:42 -0500

    new cleaned logic

--- Source/Module/deviceDetailsFetch.py
+++ Source/Module/deviceDetailsFetch.py
@@ -1,3 +1,12 @@
 # MAC Oui Identification Module
 # LAN IP and Getway Identification
 #
+
+class fetchDeviceDetails:
+
+    def __init__(self, ipObject):
+        self.mac = ipObject["Ethernet"]
+
+    def oui_identification(self):
+
+    def
--- Source/Module/pcapReader.py
+++ Source/Module/pcapReader.py
@@ -5,100 +5,59 @@ from netaddr import *
 class pcapReader():
 
     def __init__(self, filename):
-        # pcap file handle
-        self.packets = sniff(offline=filename,filter="host 192.168.15.4")
-        #wrpcap('httponly.pcap',self.packets)
-        #http = rdpcap('httponly.pcap')
-        self.packets.conversations(type="jpg", target="> test.jpg")
-        #print self.packets.summary()
-        #self.packets = rdpcap(filename)
-        #print self.packets.summary()
-        #for packet in self.packets:
-        #    print packet.summary()
-        #    print "..."
-        #self.packets.conversations(type="jpg", target="> test.jpg")
+        self.file = filename
+        self.packets = rdpcap(filename)
         self.packetDB = {}
-        #self.private_ip_segregation()
+        self.private_ip_segregation()
 
-    """
-
+# Private IP Segregation or LAN IP Identification Method
+# A LAN Map tool to plot from the perspective of LAN Hosts
+# Build a JSON Database of Private IPS
     def private_ip_segregation(self):
-        sessions = self.packets.sessions()
-        for session in sessions:
-            for each_session in sessions[session]:
-                for packet in each_session:
-                        if packet.haslayer(IP) and IPAddress(packet.getlayer(IP).src).is_private():
+                for packet in self.packets:
+                    if packet.haslayer(IP):
+                        if IPAddress(packet.getlayer(IP).src).is_private():
                             if packet.getlayer(IP).src not in self.packetDB:
                                 self.packetDB[packet.getlayer(IP).src] = {}
                             if packet.haslayer(TCP) and "TCP" not in self.packetDB[packet.getlayer(IP).src]:
-                                        self.packetDB[packet.getlayer(IP).src]["TCP"] = {}
+                                self.packetDB[packet.getlayer(IP).src]["TCP"] = {}
                             if packet.haslayer(UDP) and "UDP" not in self.packetDB[packet.getlayer(IP).src]:
-                                        self.packetDB[packet.getlayer(IP).src]["UDP"] = {}
-                           # print self.packetDB["192.168.15.4"]
-                        """
-                        if packet.haslayer(IP) and IPAddress(packet.getlayer(IP).dst).is_private():
+                                self.packetDB[packet.getlayer(IP).src]["UDP"] = {}
+                            if packet.haslayer(Ethernet) and "Ethernet" not in self.packetDB[packet.getlayer(IP).src]:
+                                self.packetDB[packet.getlayer(IP).src]["Ethernet"] = packet.getlayer(Ethernet).src
+                        if IPAddress(packet.getlayer(IP).dst).is_private():
                             if packet.getlayer(IP).dst not in self.packetDB:
                                 self.packetDB[packet.getlayer(IP).dst] = {}
-                            if packet.haslayer(TCP):
-                                    if "TCP" not in self.packetDB[packet.getlayer(IP).dst]:
-                                        self.packetDB[packet.getlayer(IP).dst]["TCP"] = {}
-                            if packet.haslayer(UDP):
-                                    if "UDP" not in self.packetDB[packet.getlayer(IP).dst]:
-                                        self.packetDB[packet.getlayer(IP).dst]["UDP"] = {}
-                                        
-                                        self.packetDB[packet.getlayer(IP).src]["UDP"]["packets"] = {}
-                                        self.packetDB[packet.getlayer(IP).src]["UDP"]["server_addresses"] = {}
-                                        self.packetDB[packet.getlayer(IP).src]["UDP"]["payloadExchange"] = {}
-                                        """
+                            if packet.haslayer(TCP) and "TCP" not in self.packetDB[packet.getlayer(IP).dst]:
+                                self.packetDB[packet.getlayer(IP).dst]["TCP"] = {}
+                            if packet.haslayer(UDP) and "UDP" not in self.packetDB[packet.getlayer(IP).dst]:
+                                self.packetDB[packet.getlayer(IP).dst]["UDP"] = {}
+                            if packet.haslayer(Ethernet) and "Ethernet" not in self.packetDB[packet.getlayer(IP).dst]:
+                                self.packetDB[packet.getlayer(IP).dst]["Ethernet"] = packet.getlayer(Ethernet).dst
 
-    def fetch_specific_protocol(self, ip, layer, protocol):
-        # Protocol Packet Store
-        #self.packetDB[protocol] = []
-        #self.payloadExchange[protocol] = []
-        #self.server_addresses[protocol] = []
-        # Protocol with Well Known Ports
-        if layer in self.packetDB[ip] and protocol not in self.packetDB[ip][layer]:
-            self.packetDB[ip][layer][protocol] = {}
-        if protocol == "HTTP":
-            port = 80
-        elif protocol == "HTTPS":
-            port = 443
-        else:
-            port = None
-        # Packet Iteration
-        sessions = self.packets.sessions()
-        for session in sessions:
-            for each_session in sessions[session]:
-                for packet in each_session:
-                    if packet.haslayer(IP): 
-                     if packet.getlayer(IP).src == ip or packet.getlayer(IP).dst == ip:
-                      if packet.haslayer(layer):
-                        if packet[layer].dport == port or packet[layer].sport == port:
-                            #print self.packetDB[ip], ip, packet.show()
-                            if "packets" not in self.packetDB[ip][layer][protocol]:
-                                self.packetDB[ip][layer][protocol]["packets"] = []
-                            self.packetDB[ip][layer][protocol]["packets"].append(packet)
-                            # Only for HTTP and HTTPS
-                            if packet.haslayer(Raw):
-                                if "payloads" not in self.packetDB[ip][layer][protocol]:
-                                    self.packetDB[ip][layer][protocol]["payloads"] = []
-                                self.packetDB[ip][layer][protocol]["payloads"].append("\n".join(packet.sprintf("{Raw:%Raw.load%}\n").split(r"\r\n")))
-                        #Destination Server Address
-                        if packet[layer].dport == port:
-                            if "server_addresses" not in self.packetDB[ip][layer][protocol]:
-                                self.packetDB[ip][layer][protocol]["server_addresses"] = []
-                            self.packetDB[ip][layer][protocol]["server_addresses"].append(packet.getlayer(IP).dst)
-                            self.packetDB[ip][layer][protocol]["server_addresses"] = list(set(self.packetDB[ip][layer][protocol]["server_addresses"]))
-                    else:
-                        return None
 
+# Sniff Packets with Filter
+    def packet_filter(self, ipaddress="", protocol="", port=""):
+        if ipaddress is not "":
+            filter = "host "+ipaddress
+        if protocol is not "":
+            filter = filter+" and "+protocol
+        if port is not "":
+            filter = filter+" and "+"port "+port
+        return sniff(offline=self.filename, filter=filter)
+
+# Populate the JSON database for specific protocols
+    def populate(self, protocol):
+        for ip in self.packetDB:
+            if protocol not in self.packetDB[ip]:
+                self.packetDB[ip][protocol] = {}
+            self.packetDB[ip][protocol] = self.packet_filter(ip, protocol)
 
 # Module Driver
 def main():
     pcapfile = pcapReader('lanExample.pcap')
-    #for ip in pcapfile.packetDB:
-    #    pcapfile.fetch_specific_protocol(ip, "TCP","HTTPS")
-    #print pcapfile.packetDB
-
+    print pcapfile.packetDB
+    pcapfile.populate("HTTP")
+    print pcapfile.packetDB
 
 main()
