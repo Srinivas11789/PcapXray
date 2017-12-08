0cfeb034c5bfb40c2c4b3816a368d9691fcbff74
srinivas11789 <srinivas11789@gmail.com>
2017-12-08 16:50:52 -0500

    add image frame for graph display

--- Source/Module/userInterface.py
+++ Source/Module/userInterface.py
@@ -10,7 +10,7 @@ class pcapXrayGui:
     def __init__(self, base):
         self.base = base
         base.title("PcapXray")
-        base.resizable(width=FALSE, height=FALSE)
+        #base.resizable(width=FALSE, height=FALSE)
         Label(base, text="PcapXray Tool - A LAN Network Analyzer")
 
         style = ttk.Style()
@@ -38,28 +38,50 @@ class pcapXrayGui:
         self.option.set('Tor')
         ttk.OptionMenu(self.SecondFrame,self.option,*options).grid(column=1, row=10,sticky="W, E")
 
-        self.ThirdFrame = ttk.Frame(base,  width=50, padding="10 10 10 10",relief= GROOVE)
+        #self.ThirdCanvas = Canvas(base)
+        self.ThirdFrame = ttk.Frame(base,  width=100, height=100, padding="10 10 10 10",relief= GROOVE)
         description = """It is a tool aimed to simplyfy the network analysis and speed the process of analysing the network traffic.\nThis prototype aims to accomplish 4 important modules,
                         \n 1. Web Traffic\n 2. Tor Traffic \n 3. Malicious Traffic \n 4. Device/Traffic Details\n\nPlease contact me @ spg349@nyu.edu for any bugs or problems !
                       """
-
-        self.label = ttk.Label(self.ThirdFrame, text="Description: \nPcapXray tools is an aid for Network Forensics or Any Network Analysis!\n"+description, style="BW.TLabel").grid(column=10, row=10,sticky="W")
+        #self.scrollHort = ttk.Scrollbar(self.ThirdFrame, orient='horizontal',command=self.ThirdCanvas.xview)
+        #self.ThirdCanvas.configure(xscrollcommand=self.scrollHort.set)
+        self.label = ttk.Label(self.ThirdFrame, text="Description: \nPcapXray tools is an aid for Network Forensics or Any Network Analysis!\n"+description, style="BW.TLabel")
+        self.label.grid(column=10, row=10,sticky="W")
+        #self.label.pack(fill=BOTH, expand=YES)
         self.ThirdFrame.grid(column=10, row=30, sticky=(N, W, E, S))
-        self.ThirdFrame.columnconfigure(10, weight=1)
-        self.ThirdFrame.rowconfigure(10, weight=1)
+        #self.ThirdFrame.pack(fill=BOTH,expand=YES)
+        self.ThirdFrame.columnconfigure(100, weight=1)
+        self.ThirdFrame.rowconfigure(100, weight=1)
+        #self.ThirdCanvas.grid(column=10, row=30, sticky=(N, W, E, S))
+        #self.scrollHort.grid(column=10, row=30, sticky='ew')
+
 
 
     def pcap_analyse(self):
-        def progress_bar():
-            self.progressbar.start()
-        t = threading.Thread(None, progress_bar, ())
-        t.start()
+        #def progress_bar():
+        self.progressbar.start()
+        #t = threading.Thread(None, progress_bar, ())
+        #t.start()
+        self.base.update()
         capture_read = pcapReader.pcapReader(self.pcap_file.get())
-        diag1 = threading.Thread(plotLanNetwork.plotLan, (capture_read.packetDB, "network111", self.option.get()))
-        while diag1.is_alive():
-            self.base.update()
-        image = ImageTk.PhotoImage(Image.open("network111Tor.png"))
-        self.label = ttk.Label(self.ThirdFrame, image=image, style="BW.TLabel").grid(column=10, row=10,sticky="W")
+        print "ReadingDone"
+        #diag1 = threading.Thread(target=plotLanNetwork.plotLan, args=(capture_read.packetDB, "network111", self.option.get(),))
+        #diag1.start()
+        #while diag1.is_alive():
+        #    self.base.update()
+        self.label.grid_forget()
+        #image_window = ttk.Frame(self.ThirdCanvas)
+        #self.ThirdCanvas.create_window(0,0,window=image_window,anchor='nw')
+        image = ImageTk.PhotoImage(Image.open("network123.png").resize((700,700)))
+        self.label = ttk.Label(self.ThirdFrame,image=image,relief=RAISED)
+        self.label.image = image
+        #self.label.grid(sticky="W")
+        self.label.pack(fill=X,expand=1)
+        #self.label.pack()
+        #w = Canvas(self.ThirdFrame, width=50,height=50)
+        #w.create_image(0,0,image=image)
+        #self.label.pack(fill=BOTH, expand=1)
+        #self.base.update()
         self.progressbar.stop()
 
 def main():
