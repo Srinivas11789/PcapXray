78cabf22da5d9412fbbb12b08a69947eb06aa1d9
srinivas11789 <srinivas11789@gmail.com>
2018-02-24 23:58:26 -0500

    Build tests - Initial basic testcase

---
+++ Tests/test_pcapReader.py
@@ -0,0 +1,16 @@
+# Initial Basic Test - Assure proper pcap reading of the example/test.pcap file
+import sys
+print sys.path[0]
+if sys.path[0]:
+    sys.path.insert(0, sys.path[0]+'/../Source/Module/')
+else:
+    sys.path.insert(0,'/../Source/Module/')
+
+import pcapReader
+
+def test_pcapreader():
+    pcapfile = pcapReader.pcapReader(sys.path[0]+'examples/test.pcap')
+    if pcapfile.packetDB:
+        assert True
+
+test_pcapreader()
