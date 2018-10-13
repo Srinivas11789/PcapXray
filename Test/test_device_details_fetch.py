57321dee42a03e297d726a3b36a2bd033be14669
Srinivas <spg349@nyu.edu>
2018-10-13 04:16:42 -0700

    :construction: add test file for module device details

---
+++ Test/test_device_details_fetch.py
@@ -0,0 +1,11 @@
+# Test file for deviceDetailsFetch module
+"""
+The Module as of now has the following provisions:
+* Obtained the ethernet address of all the host present in the network < obtained from packetsDb > 
+* Identify the OUI 
+* Format the response and Update the packetDb with corresponding information
+"""
+
+# Testcases
+
+
