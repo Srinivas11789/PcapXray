3da8ae2891de536146f477e54cc750f6609fcfec
Srinivas P G <spg349@nyu.edu>
2019-08-02 00:39:07 -0700

    Revert "Covert communication"

--- Source/Module/malicious_traffic_identifier.py
+++ Source/Module/malicious_traffic_identifier.py
@@ -3,7 +3,6 @@ import memory
 
 # Custom Module Import
 import pcap_reader
-import communication_details_fetch
 
 # Library Import
 
@@ -21,46 +20,12 @@ class maliciousTrafficIdentifier:
         very_well_known_ports = [443] # used to differentiate possible mal vs serious mal
         well_known_ports = [20, 21, 22, 23, 25, 53, 69, 80, 161, 179, 389, 443]
         # Currently whitelist all the ports
-        if not communication_details_fetch.trafficDetailsFetch.is_multicast(src) and not communication_details_fetch.trafficDetailsFetch.is_multicast(dst):
-            if (dst in memory.destination_hosts and memory.destination_hosts[dst]["domain_name"] == "NotResolvable") or port > 1024:
-                return 1
-        return 0
+        if (dst in memory.destination_hosts and memory.destination_hosts[dst]["domain_name"] == "NotResolvable") or port > 1024:
+            return 1
+        else:
+            return 0
 
     # TODO: Covert communication module --> Add here
-    # * Only add scapy first
-
-    # Covert Detection Algorithm
-    @staticmethod
-    def covert_traffic_detection(packet):
-        # covert ICMP - icmp tunneling
-        tunnelled_protocols = ["DNS", "HTTP"]
-
-        if "IP" in packet:
-            if communication_details_fetch.trafficDetailsFetch.is_multicast(packet["IP"].src) or communication_details_fetch.trafficDetailsFetch.is_multicast(packet["IP"].dst):
-                return 0
-
-        if "ICMP" in packet:
-            if "TCP in ICMP" in packet or "UDP in ICMP" in packet or "DNS" in packet:
-                #print(packet.show())
-                return 1
-            elif "padding" in packet:
-                return 1
-            elif filter(lambda x: x in str(packet["ICMP"].payload), tunnelled_protocols):
-                return 1
-        elif "DNS" in packet:
-            #print(packet["DNS"].qd.qname)
-            try:
-                if communication_details_fetch.trafficDetailsFetch.dns(packet["DNS"].qd.qname.strip()) == "NotResolvable":
-                    return 1
-                elif len(filter(str.isdigit, str(packet["DNS"].qd.qname).strip())) > 8:
-                    return 1
-            except:
-                pass
-        return 0
-    
-    # Covert payload prediction algorithm
-    ##@staticmethod
-    ##def covert_payload_prediction(session):
 
 def main():
     cap = pcap_reader.PcapEngine('examples/torExample.pcap', "scapy")
--- Source/Module/memory.py
+++ Source/Module/memory.py
@@ -3,14 +3,6 @@
 
 global packet_db
 packet_db = {}
-
-# Schema
-# Key for access is MAC Address/IP Address
-# * Initially had mac address (assuming unique) as the only key
-#   - CTF problems sometime cause a scenario of same mac with different IP address so segregated this
-# * Otherwise each key holds
-#   - Mac Vendor
-#   - Ip address
 global lan_hosts
 lan_hosts = {}
 global destination_hosts
